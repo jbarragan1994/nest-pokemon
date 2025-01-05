bfd5a7d67474796833bfe8f9b3170d2c1961564a
Barragan <jcbarragan1994@gmail.com>
2025-01-05 01:25:40 -0400

    fix: update log message format for application startup

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   );
   await app.listen(process.env.PORT ?? 3000);
 
-  console.log(`Application is running on: ${process.env.PORT}`);
+  console.log(`Application is running on ${process.env.PORT}`);
 }
 bootstrap();
